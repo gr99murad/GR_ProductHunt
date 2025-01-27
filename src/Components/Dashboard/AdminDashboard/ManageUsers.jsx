c959b4fc97a98c2c88ef7befbcadd7d7bc681a76
gr99murad <golamrabbanimurad@gmail.com>
2025-01-27 16:14:17 +0600

    Added only admin change role

--- src/Components/Dashboard/AdminDashboard/ManageUsers.jsx
+++ src/Components/Dashboard/AdminDashboard/ManageUsers.jsx
@@ -1,16 +1,29 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const ManageUsers = () => {
     const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [currentUser, setCurrentUser] = useState(null);
+    const navigate = useNavigate();
+    
+
 
     useEffect(() => {
         const fetchUsers = async () => {
             try{
                 const response = await axios.get('http://localhost:5000/users');
-                setUsers(response.data);
+                
+                
+                    setUsers(response.data);
+                    setLoading(false);
+
+                
+                
             }catch(error){
                 console.error('Error fetching users', error);
+                setLoading(false);
             }
         };
         fetchUsers();
@@ -53,9 +66,13 @@ const ManageUsers = () => {
                                 <td className='border border-gray-300 px-4 py-2'>{user.email}</td>
                                 <td className='border border-gray-300 px-4 py-2'>{user.role || 'user'}</td>
                                 <td className='border border-gray-300 px-4 py-2 space-x-2'>
-                                    <button className='btn bg-blue-500 text-white rounded' onClick={() => updateUserRole(user.email, 'moderator')} disabled={user.role === 'moderator'}>Make Moderator</button>
+                                {user.role !== 'admin' && (
+                                        <>
+                                          <button className='btn bg-blue-500 text-white rounded' onClick={() => updateUserRole(user.email, 'moderator')} disabled={user.role === 'moderator'}>Make Moderator</button>
 
-                                    <button className='btn bg-green-500 text-white px-3 py-1 rounded' onClick={() => updateUserRole(user.email, 'admin')} disabled={user.role === 'admin'}>make Admin</button>
+                                          <button className='btn bg-green-500 text-white px-3 py-1 rounded' onClick={() => updateUserRole(user.email, 'admin')} disabled={user.role === 'admin'}>make Admin</button>
+                                        </>
+                                    )}
                                 </td>
                             </tr>
                         ))
@@ -63,7 +80,7 @@ const ManageUsers = () => {
                     ):(
 
                         <tr>
-                            <td className='border px-4 py-2 text-center'>No reported Products found</td>
+                            <td className='border px-4 py-2 text-center'>No users found</td>
                         </tr>
                     )}
                     
